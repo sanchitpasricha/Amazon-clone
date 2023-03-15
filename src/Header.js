e0f95f49c16a51d17cb5014cfabd262bc558b80d
sanchitpasricha <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>
2023-03-15 12:16:59 +0530

    comment down what you require

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div>
+      {/* logo on the left of the header*/}
+      {/* search bar*/}
+      {/* 3 links*/}
+      {/* Basket icon with number */}
+    </div>
+  );
+}
+
+export default Header;

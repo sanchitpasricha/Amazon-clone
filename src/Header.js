16c2bacebd4e9424f8374182661f30da1a124d17
sanchitpasricha <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>
2023-03-15 14:33:22 +0530

    header component completed

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Header.css";
 import { Link } from "react-router-dom";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 function Header() {
   return (
@@ -12,6 +14,41 @@ function Header() {
           src="https://www.nicepng.com/png/full/16-167642_amazon-logo-amazon-logo-white-text.png"
         ></img>
       </Link>
+
+      <div className="header__search">
+        <input type="text" className="header__searchInput" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+
+      <div className="header__nav">
+        <Link to="/login" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Hello,Sanchit</span>
+            <span className="header__optionLineTwo">Sign in</span>
+          </div>
+        </Link>
+
+        <Link to="/" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Returns</span>
+            <span className="header__optionLineTwo">& Orders</span>
+          </div>
+        </Link>
+
+        <Link to="/" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Your</span>
+            <span className="header__optionLineTwo">Prime</span>
+          </div>
+        </Link>
+
+        <Link to="/checkout" className="header__link">
+          <div className="header__optionBasket">
+            <ShoppingCartIcon />
+            <span className="header__optionLineTwo header__basketCount">0</span>
+          </div>
+        </Link>
+      </div>
     </nav>
   );
 }

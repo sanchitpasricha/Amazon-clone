44711104696474b775b1968adf039f82211c9bb7
sanchitpasricha <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>
2023-03-15 12:00:15 +0530

    setting up react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Ready for dev, project cleaned !</h1>
-    </div>
+    <Router>
+      {/* React router gives the app the feel of desktop apps so you're switching b/w pages 
+    without talking to the server thus the page does'nt reload*/}
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <h1>Checkout</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Log in page</h1>
+          </Route>
+          <Route path="/">
+            <h1>Home Page !!!</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 

61dd2364e35d8cbe4b9810e37a71bfc9f1b99a74
sanchitpasricha <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>
2023-03-16 18:02:45 +0530

    product tiles

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./home.css";
+import Product from "./Product";
 
 function Home() {
   return (
@@ -9,6 +10,56 @@ function Home() {
         alt=""
         src="https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61gvdXf4+SL._SX3000_.jpg"
       />
+      <div className="home__row">
+        <Product
+          id="12345"
+          title="Just Keep Buying : The art of creting wealth"
+          price={11.96}
+          rating={5}
+          image="https://imgv2-1-f.scribdassets.com/img/word_document/565861428/original/216x287/3e2fe635f5/1672772495?v=1"
+        />
+        <Product
+          id="12345"
+          title="Just Keep Buying : The art of creting wealth"
+          price={11.96}
+          rating={5}
+          image="https://imgv2-1-f.scribdassets.com/img/word_document/565861428/original/216x287/3e2fe635f5/1672772495?v=1"
+        />
+      </div>
+
+      <div className="home__row">
+        <Product
+          id="12345"
+          title="Just Keep Buying : The art of creting wealth"
+          price={11.96}
+          rating={5}
+          image="https://imgv2-1-f.scribdassets.com/img/word_document/565861428/original/216x287/3e2fe635f5/1672772495?v=1"
+        />
+        <Product
+          id="12345"
+          title="Just Keep Buying : The art of creting wealth"
+          price={11.96}
+          rating={5}
+          image="https://imgv2-1-f.scribdassets.com/img/word_document/565861428/original/216x287/3e2fe635f5/1672772495?v=1"
+        />
+        <Product
+          id="12345"
+          title="Just Keep Buying : The art of creting wealth"
+          price={11.96}
+          rating={5}
+          image="https://imgv2-1-f.scribdassets.com/img/word_document/565861428/original/216x287/3e2fe635f5/1672772495?v=1"
+        />
+      </div>
+
+      <div className="home__row">
+        <Product
+          id="12345"
+          title="Just Keep Buying : The art of creting wealth"
+          price={11.96}
+          rating={5}
+          image="https://imgv2-1-f.scribdassets.com/img/word_document/565861428/original/216x287/3e2fe635f5/1672772495?v=1"
+        />
+      </div>
     </div>
   );
 }

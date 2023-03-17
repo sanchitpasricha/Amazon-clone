74f96f94ec6f21175b235505d8d7aac39597a87a
sanchitpasricha <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>
2023-03-17 09:33:08 +0530

    setting up React Context

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { StateProvider } from "./StateProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <StateProvider initialState={""} reducer={""}>
+    <App />
+  </StateProvider>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

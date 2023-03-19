a5a971c6fe67971f6de9a794cb854853c8f8d850
sanchitpasricha <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>
2023-03-19 10:27:38 +0530

    partitioning right and left

--- src/Checkout.js
+++ src/Checkout.js
@@ -2,36 +2,44 @@ import React from "react";
 import { useStateValue } from "./StateProvider";
 import "./Checkout.css";
 import CheckoutProduct from "./CheckoutProduct";
+import Subtotal from "./Subtotal";
 
 function Checkout() {
   const [{ basket }] = useStateValue();
 
   return (
     <div className="checkout">
-      <img
-        className="checkout__ad"
-        alt=""
-        src="https://m.media-amazon.com/images/G/01/FireTV/Inline/IDB_RatingLabel_NA._TTW_.jpg"
-      ></img>
-      {basket?.length === 0 ? (
-        <div>
-          <h1>Your shopping basket is empty.</h1>
-          <p>
-            You have no items in your cart to add on click on "Add to cart"
-            below the items listed.
-          </p>
-        </div>
-      ) : (
-        <div>
-          {basket.map((item) => (
-            <CheckoutProduct
-              id={item.id}
-              title={item.title}
-              image={item.image}
-              price={item.price}
-              rating={item.rating}
-            />
-          ))}
+      <div className="checkout__left">
+        <img
+          className="checkout__ad"
+          alt=""
+          src="https://m.media-amazon.com/images/G/01/FireTV/Inline/IDB_RatingLabel_NA._TTW_.jpg"
+        ></img>
+        {basket?.length === 0 ? (
+          <div>
+            <h1>Your shopping basket is empty.</h1>
+            <p>
+              You have no items in your cart to add on click on "Add to cart"
+              below the items listed.
+            </p>
+          </div>
+        ) : (
+          <div>
+            {basket.map((item) => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+      {basket.length > 0 && (
+        <div className="checkout__right">
+          <Subtotal />
         </div>
       )}
     </div>
